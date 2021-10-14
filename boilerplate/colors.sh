ae6ab8c8d2ef749caaf6d1569374fd4bdb2f6f38
Ivan Boothe <ivan@rootwork.org>
2021-10-14 14:51:30 -0700

    Adding boilerplate code folder
    Signed-off-by: Ivan Boothe <ivan@rootwork.org>

---
+++ boilerplate/colors.sh
@@ -0,0 +1,33 @@
+# Colors.
+#
+# Sources:
+#
+# https://techstop.github.io/bash-script-colors/
+# https://stackoverflow.com/a/4332530
+
+# ANSI (for echo/echo -e)
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
+
+# tput (for printf)
+
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+bold=$(tput bold)
+reset=$(tput sgr0)
+reverse=$(tput smso)

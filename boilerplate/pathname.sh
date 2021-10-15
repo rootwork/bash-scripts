7ee98c696665fd2fdec79edeeba45c85a7c1ccfe
Ivan Boothe <ivan@rootwork.org>
2021-10-15 13:08:01 -0700

    Adding file operations to pathname
    Signed-off-by: Ivan Boothe <ivan@rootwork.org>

--- boilerplate/pathname.sh
+++ boilerplate/pathname.sh
@@ -10,3 +10,12 @@
 # Sanitize directory name
 path=$1
 [[ "$path" =~ ^[./].*$ ]] || path="./$path"
+
+# Dissect and examine filename
+file=$1
+name="${1%.*}"
+ext="${1##*.}"
+if [[ ! $file ]]; then
+  usage >&2
+  error_exit "Filename must be provided."
+fi

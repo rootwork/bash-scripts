ead7796bb32eb36b577f959d924c12857ed9206a
Ivan Boothe <git@rootwork.org>
2021-11-29 13:33:00 -0800

    fix optarg
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- videos/vidcap.sh
+++ videos/vidcap.sh
@@ -149,7 +149,9 @@ while getopts :xo-:qh OPT; do
     OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
     OPTARG="${OPTARG#=}"      # remove assigning `=`
   else
-    OPTARG="${OPTARG#=}" # if short option, just remove assigning `=`
+    if [[ "$OPTARG" ]]; then
+      OPTARG="${OPTARG#=}" # if short option, just remove assigning `=`
+    fi
   fi
   case "$OPT" in
     h | help)

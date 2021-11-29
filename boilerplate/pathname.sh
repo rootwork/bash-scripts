2309c284b0b1dee23abda20b33194383092c471b
Ivan Boothe <git@rootwork.org>
2021-11-29 13:13:06 -0800

    updating vars to comply with bash strict
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- boilerplate/pathname.sh
+++ boilerplate/pathname.sh
@@ -8,14 +8,18 @@
 # https://linuxcommand.org/lc3_adv_standards.php
 
 # Sanitize directory name
-path=$1
+path=${1:-}
+if [[ -z "$path" ]]; then
+  usage >&2
+  error_exit "Directory path must be provided."
+fi
 [[ "$path" =~ ^[./].*$ ]] || path="./$path"
 
 # Dissect and examine filename
-file=$1
-name="${1%.*}"
-ext="${1##*.}"
-if [[ ! $file ]]; then
+file=${1:-}
+if [[ -z "$file" ]]; then
   usage >&2
   error_exit "Filename must be provided."
 fi
+name="${1%.*}"
+ext="${1##*.}"
